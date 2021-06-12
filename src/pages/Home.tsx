9bdc6263b872f9d70a615c44ecd2452b9f436ae7
afsonk <q8737501@gmail.com>
2021-06-12 20:29:56 +0300

    styled app using ionic components

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,124 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+    IonContent,
+    IonIcon,
+    IonPage,
+    IonTitle,
+    IonCard,
+    IonButton,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonImg,
+    IonText,
+} from '@ionic/react'
+
+// @ts-ignore
+import image from '../assets/img/man.png'
+// @ts-ignore
+import list from '../assets/img/Vector.svg'
+// @ts-ignore
+import calen from '../assets/img/Union.svg'
+import './Home.css'
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
 
-export default Home;
+    return (
+        <IonPage>
+            <IonContent>
+                <IonGrid class={'main-grid'}>
+                    <IonRow>
+                        <IonTitle class="ion-text-center">
+                            <h3>
+                                Алексей Карачинский
+                            </h3>
+                        </IonTitle>
+                    </IonRow>
+
+                    <IonCard class={'master-card'}>
+                        <IonRow class={'picture'}>
+                            <IonCol>
+                                <IonImg class={'background'}
+                                        src={image}
+                                        alt="avatar"
+                                />
+                            </IonCol>
+                            <IonCol class="ion-text-center">
+                                <IonText>
+                                    Длительность
+                                    консультации
+                                </IonText>
+                                <h2>50 минут</h2>
+                            </IonCol>
+                        </IonRow>
+                    </IonCard>
+                    <IonRow class={'heading'}>
+                        <IonCol size='9'>
+                            <IonText >
+                                Возможная дата
+                            </IonText>
+                        </IonCol>
+                        <IonCol>
+                            <IonIcon src={list} size='9'></IonIcon>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow class={'slides'}>
+                        <IonCard class={"date-item ion-text-center active"}>
+                            <p>Сегодня</p>
+                            <h2>26</h2>
+                        </IonCard>
+                    </IonRow>
+                    <IonRow class={'heading'}>
+                        <IonCol size='12'>
+                            <IonText>
+                                Свободное время
+                            </IonText>
+                        </IonCol>
+                    </IonRow>
+                    <IonRow class={'slides'}>
+                        <IonCard class={'time-item ion-text-center'}>
+                            11:20
+                        </IonCard>
+                        <IonCard class={'time-item ion-text-center active'}>
+                            11:20
+                        </IonCard>
+                        <IonCard class={'time-item ion-text-center'}>
+                            11:20
+                        </IonCard>
+                        <IonCard class={'time-item ion-text-center'}>
+                            11:20
+                        </IonCard>
+                        <IonCard class={'time-item ion-text-center'}>
+                            11:20
+                        </IonCard>
+                        <IonCard class={'time-item ion-text-center'}>
+                            11:20
+                        </IonCard>
+                    </IonRow>
+                    <IonCard class={'bottom-card'}>
+                        <IonRow class={'booking'}>
+                            <IonCol>
+                                <div className={"ion-text-center"}>
+                                    <p>Дата</p>
+                                    <h2>26 мая</h2>
+                                </div>
+                            </IonCol>
+                            <IonCol class="ion-text-center">
+                                <div className={"ion-text-center"}>
+                                    <p>Время</p>
+                                    <h2>18:30</h2>
+                                </div>
+                            </IonCol>
+                        </IonRow>
+                        <IonRow class="ion-justify-content-center date-time">
+                            <IonButton class={'check-btn'} color="secondary">
+                                ЗАПИСАТЬСЯ НА БЕСПЛАТНУЮ ВСТРЕЧУ
+                            </IonButton>
+                        </IonRow>
+                    </IonCard>
+                </IonGrid>
+            </IonContent>
+        </IonPage>
+    )
+}
+
+export default Home
